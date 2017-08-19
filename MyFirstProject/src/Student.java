ce69eb337e972b93a46defbdfc114e3efbe3447b
KRISHNA <maddelakrishnakumar@gmail.com>
2017-08-19 10:22:29 +0530

    second commit

---
+++ MyFirstProject/src/Student.java
@@ -0,0 +1,23 @@
+
+public class Student {
+	String name,email;
+
+	String eid;
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+
+}
